926ae767cacb98ec64a52deeadb2e306a14d43aa
cryptominingproject786-svg <cryptominingproject786-svg@github.com>
2026-03-08 09:44:58 -0700

    admin dashboard error complete

--- src/app/admin/dashbord/page.jsx
+++ src/app/admin/dashbord/page.jsx
@@ -1,12 +1,12 @@
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// import AdminClient from "./AdminClient";
+import AdminClient from "./AdminClient";
 
-// export default function AdminPage() {
-//     // Server component: render the client-side admin UI
-//     return (
-//         <div>
-//             <AdminClient />
-//         </div>
-//     );
-// }
+export default function AdminPage() {
+    // Server component: simply render the client-side admin UI
+    return (
+        <div>
+            <AdminClient />
+        </div>
+    );
+}
