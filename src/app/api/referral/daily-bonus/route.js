4300138040c66a2aca07a354d047ebf9d44b8bd4
cryptominingproject786-svg <cryptominingproject786-svg@github.com>
2026-04-20 06:28:20 -0700

    dynamic error is fixed

--- src/app/api/referral/daily-bonus/route.js
+++ src/app/api/referral/daily-bonus/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import connectDB from "../../../lib/mongoDb";
 import { processReferralDailyBonuses } from "../../../lib/referralDailyBonus";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req) {
     try {
         const url = new URL(req.url);
